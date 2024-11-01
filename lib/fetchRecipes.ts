e1d0e2eb779704c05c768968cb8ea096b036492d
FEDERICO ILAN OSTROVSKY <47434260@ort>
2024-11-01 09:46:12 -0300

    Diablo

--- lib/fetchRecipes.ts
+++ lib/fetchRecipes.ts
@@ -2,11 +2,12 @@ import axios from 'axios';
 import { API_KEY, recipes_url } from './constants';
 
 
-export const searchRecipes = async () => {
+export const searchRecipes = async (chars) => {
   try {
     const response = await axios.get(recipes_url, {
       params: {
         apiKey: API_KEY,
+        query: chars
       }
     });
     return (response.data.results)
